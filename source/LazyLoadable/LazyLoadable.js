4508350d80b99f7b4f6559af54c044fb93c181c0
Oscar Barrera <oscbco@gmail.com>
2019-11-07 12:25:31 -0600

    Changed never reassigned variable to a const instead

--- source/LazyLoadable/LazyLoadable.js
+++ source/LazyLoadable/LazyLoadable.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import importedComponent from 'react-imported-component';
 
 export default function LazyLoadable ({ importComponent, isVisible, ...passProps }) {
-  let rendered = useRef(false);
-  let LoadableComp = useRef(false);
+  const rendered = useRef(false);
+  const LoadableComp = useRef(false);
   useEffect(() => {
     LoadableComp.current = importedComponent(importComponent);
     LoadableComp.current.preload();
